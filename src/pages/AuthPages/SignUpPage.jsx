4bcf9aebe7c63c1cef85504646f20dde97ef5963
dotw0xff <dotw0xff@gmail.com>
2024-05-22 16:21:03 +0900

    feat: 회원가입 페이지의 데이터 검증 로직을 react-hook-form으로 재구현함(#8)

--- src/pages/AuthPages/SignUpPage.jsx
+++ src/pages/AuthPages/SignUpPage.jsx
@@ -1,120 +1,121 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Controller, useForm } from "react-hook-form";
 import "./auth.css";
 import Logo from "../../assets/images/logo/logo.svg";
 import HideIcon from "../../assets/images/icons/eye-invisible.svg"
 
-function SignUpPage() {
+function LoginPage() {
+  const {
+    control,
+    handleSubmit,
+  } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(JSON.stringify(data));
+  }
+
   return (
-    <div class="auth-container">
-      <Link to="/" class="logo-home-link" aria-label="홈으로 이동">
+    <div className="auth-container">
+      <Link to="/" className="logo-home-link" aria-label="홈으로 이동">
         <img
           src={Logo}
           alt="판다마켓 로고"
         />
       </Link>
 
-      <form id="signupForm" method="post">
-        <div class="input-item">
-          <label for="email">이메일</label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            placeholder="이메일을 입력해 주세요"
-            required
-          />
+      <form id="loginForm" onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="email"
+          control={control}
+          rules={{
+            required: "이메일을 입력해주세요",
+            pattern: {
+              value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: "잘못된 이메일 형식입니다."
+            }
+          }}
+          render = {({ field: { value, onChange }, fieldState: { error } }) => {
+            return (
+              <div className="input-item">
+                <label htmlFor="email">이메일</label>
+                <input
+                  id="email"
+                  name="email"
+                  placeholder="이메일을 입력해주세요"
+                  value={value}
+                  onChange={onChange}
+                />
 
-          <span id="emailEmptyError" class="error-message"
-            >이메일을 입력해 주세요</span
-          >
-          <span id="emailInvalidError" class="error-message"
-            >잘못된 이메일 형식입니다</span
-          >
-        </div>
+                { error && 
+                  <span className="error-message">
+                    {error?.message}
+                  </span>
+                }
+              </div>
+            );
+          }}
+        />
 
-        <div class="input-item">
-          <label for="nickname">닉네임</label>
-          <input
-            id="nickname"
-            name="nickname"
-            type="text"
-            placeholder="닉네임을 입력해 주세요"
-            required
-          />
-          <span id="nicknameEmptyError" class="error-message"
-            >닉네임을 입력해 주세요</span
-          >
-        </div>
+        <Controller
+          name="password"
+          control={control}
+          rules={{
+            required: "비밀번호를 입력해주세요",
+            minLength: {
+              value: 8,
+              message: '비밀번호를 8자 이상 입력해주세요'
+            }
+          }}
+          render = {({ field: { value, onChange }, fieldState: { error } }) => {
+            return (
+              <div className="input-item">
+                <label htmlFor="password">비밀번호</label>
+                <div className="input-wrapper">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder="비밀번호를 입력해 주세요"
+                    value={value}
+                    onChange={onChange}
+                  />
+                  <button
+                    type="button"
+                    className="password-toggle-button"
+                    aria-label="비밀번호 보기"
+                  >
+                    <img
+                      className="password-toggle-icon"
+                      src={HideIcon}
+                      alt="비밀번호 숨김 상태 아이콘"
+                    />
+                  </button>
+                </div>
+                { error && 
+                  <span className="error-message">
+                    {error?.message}
+                  </span>
+                }
+              </div>
+            );
+          }}
+        />
 
-        <div class="input-item">
-          <label for="password">비밀번호</label>
-          <div class="input-wrapper">
-            <input
-              id="password"
-              name="password"
-              type="password"
-              placeholder="비밀번호를 입력해 주세요"
-              required
-            />
-            <button
-              type="button"
-              class="password-toggle-button"
-              aria-label="비밀번호 보기"
-            >
-              <img
-                class="password-toggle-icon"
-                src={HideIcon}
-                alt="비밀번호 숨김 상태 아이콘"
-              />
-            </button>
-          </div>
-          <span id="passwordEmptyError" class="error-message"
-            >비밀번호를 입력해 주세요</span
-          >
-          <span id="passwordInvalidError" class="error-message"
-            >비밀번호를 8자 이상 입력해 주세요</span
-          >
-        </div>
-
-        <div class="input-item">
-          <label for="passwordConfirmation">비밀번호 확인</label>
-          <div class="input-wrapper">
-            <input
-              id="passwordConfirmation"
-              name="passwordConfirmation"
-              type="password"
-              placeholder="비밀번호를 다시 한 번 입력해 주세요"
-              required
-            />
-            <button
-              type="button"
-              class="password-toggle-button"
-              aria-label="비밀번호 보기"
-            >
-              <img
-                class="password-toggle-icon"
-                src={HideIcon}
-                alt="비밀번호 숨김 상태 아이콘"
-              />
-            </button>
-          </div>
-          <span id="passwordConfirmationInitError" class="error-message"
-            >먼저 조건에 맞는 비밀번호를 입력해 주세요</span
-          >
-          <span id="passwordConfirmationError" class="error-message"
-            >비밀번호가 일치하지 않습니다</span
-          >
-        </div>
-
-        <button type="submit" class="button pill-button full-width">
-          회원가입
+        <button type="submit" className="button pill-button full-width">
+          로그인
         </button>
       </form>
 
-      <div class="social-login-container">
+      <div className="social-login-container">
         <h3>간편 로그인하기</h3>
-        <div class="social-login-links-container">
+        <div className="social-login-links-container">
           <a
             href="https://www.google.com/"
             target="_blank"
@@ -138,11 +139,11 @@ function SignUpPage() {
         </div>
       </div>
 
-      <div class="auth-switch">
-        이미 회원이신가요? <Link to="/login">로그인</Link>
+      <div className="auth-switch">
+        판다마켓이 처음이신가요? <Link to='/signup'>회원가입</Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default SignUpPage;
+export default LoginPage;
